3db1c219b81e842cbc2641aff2b963016a28ca55
Alexis <alexismenchaca2005@gmail.com>
2026-09-21 19:29:42 -0300

    Pagina bienvenida impl

---
+++ Pages/PaginaBievenida.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarea4Net.Pages;
+
+public class PaginaBienvenidaModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
